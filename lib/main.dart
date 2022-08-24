68dd54bf00120efe7d3e874a3d8c8bec7cad8575
makairodewa <makairodewa@gmail.com>
2022-08-25 00:25:10 +0700

    generate splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:finance/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main(List<String> args) {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
